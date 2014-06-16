b6b7cc3b58651828963a8a22545b498ac6d8738b
Daniel Sundy <dansundy@gmail.com>
2014-06-15 22:10:33 -0400

    Update gulpfile to create WP template header.

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,51 @@
+var data         = require('./package.json');
+
 var gulp         = require('gulp'),
-    
     autoprefixer = require('gulp-autoprefixer'),
-    minifycss    = require('gulp-cssmin'),
+    replace      = require('gulp-replace'),
     sass         = require('gulp-ruby-sass')
     
 
 
-var src = {
-  base: './src',
-  styles: './src/sass'
-}
-
-var deploy = {
-  base: './deploy'
+var path = {
+  src: {
+    base: './src',
+    styles: './src/sass'
+  },
+  deploy: {
+    base: './deploy'
+  }
 }
 
 /** 
  * Functions 
  */
-var runStyles = function(opts) {
-  return gulp.src(src.styles + '/style.sass')
-    .pipe(sass({ style: opts.output, compass: true }))
+var styles = function(env) {
+  var source = path.src.styles + '/*.sass';
+  var output = env === 'src' ? 'expanded' : 'compressed';
+  return gulp.src(source)
+    .pipe(sass({ style: output, compass: true }))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(gulp.dest(opts.location));
+    .pipe(replace('{{themeURI}}', data.homepage))
+    .pipe(replace('{{author}}', data.author.name))
+    .pipe(replace('{{authorURI}}', data.author.url))
+    .pipe(replace('{{description}}', data.description))
+    .pipe(replace('{{version}}', data.version))
+    .pipe(replace('{{textDomain}}', data.name))
+    .pipe(replace('{{themeName}}', function() {
+      var string = data.config.title;
+      if (env === 'src') {
+        string += ' (Development)';
+      }
+      return string;
+    }))
+    .pipe(gulp.dest(path[env].base));
 }
 
 /**
  * Tasks
  */
 gulp.task('styles', function() {
-  runStyles({output: 'expanded', location: src.base});
-  runStyles({output: 'compressed', location: deploy.base});
+  styles('src');
+  styles('deploy');
 });
